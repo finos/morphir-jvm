8db779744e9443bd9d50f300b2dd41fc902629b9
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-03-04 23:31:10 -0500

    Flesh out the IR (#6)
    * Addd millw script for Windows builds
    
    * Add additional test-case
    
    * Create basic Name type #1
    
    * Add the Path type - #1
    
    * Add more to the IR

---
+++ core/src/com/morganstanley/morphir/ir/FQName.scala
@@ -0,0 +1,9 @@
+package com.morganstanley.morphir.ir
+
+case class FQName(packagePath: Path, modulePath: Path, localName: Name) {}
+
+object FQName {
+  val fQName = (packagePath: Path) =>
+    (modulePath: Path) =>
+      (localName: Name) => FQName(packagePath, modulePath, localName)
+}
--- core/src/com/morganstanley/morphir/ir/advanced/Type.scala
+++ core/src/com/morganstanley/morphir/ir/advanced/Type.scala
@@ -1,10 +1,21 @@
 package com.morganstanley.morphir.ir.advanced
-import com.morganstanley.morphir.ir.Name
+import com.morganstanley.morphir.ir.{FQName, Name}
 
 sealed trait Type[+Extra]
 object Type {
   case class Variable[A](name: Name, extra: A) extends Type[A]
+  case class Reference[A](
+      typeName: FQName,
+      typeParameters: List[Type[A]],
+      extra: A
+  ) extends Type[A]
+
+  case class Tuple[A](elementTypes: List[Type[A]], extra: A) extends Type[A]
+  case class Record[A](fieldTypes: List[Field[A]], extra: A) extends Type[A]
+  case class ExtensibleRecord[A]() extends Type[A]
   case class Function[A](argType: Type[A], returnType: Type[A], extra: A)
       extends Type[A]
   case class Unit[A](extra: A) extends Type[A]
+
+  case class Field[A](name: Name, fieldType: Type[A])
 }
