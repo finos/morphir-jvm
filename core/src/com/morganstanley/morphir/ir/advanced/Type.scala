543a2f47e2fc2d6d20595a8073ba0a19572c8862
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-03-04 20:03:32 -0500

    Creating the initial IR (#5)
    * Add millw script for Windows builds
    
    * Add additional test-case
    
    * Create basic Name type #1

--- core/src/com/morganstanley/morphir/ir/advanced/Type.scala
+++ core/src/com/morganstanley/morphir/ir/advanced/Type.scala
@@ -1,7 +1,10 @@
 package com.morganstanley.morphir.ir.advanced
+import com.morganstanley.morphir.ir.Name
 
 sealed trait Type[+Extra]
 object Type {
-    case class Function[A](argType:Type[A], returnType:Type[A], extra:A) extends Type[A]
-    case class Unit[A](extra:A) extends Type[A]
+  case class Variable[A](name: Name, extra: A) extends Type[A]
+  case class Function[A](argType: Type[A], returnType: Type[A], extra: A)
+      extends Type[A]
+  case class Unit[A](extra: A) extends Type[A]
 }
