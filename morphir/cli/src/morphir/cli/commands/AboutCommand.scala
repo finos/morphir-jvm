6864741a30d93dc142cd6b1911f0bac0d35c6818
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-03-12 17:53:29 -0400

    Working on grpc integration

--- morphir/cli/src/morphir/cli/commands/AboutCommand.scala
+++ morphir/cli/src/morphir/cli/commands/AboutCommand.scala
@@ -1,14 +1,36 @@
 package morphir.cli.commands
 
 import zio._
-import zio.console
+import zio.console._
+import zio.clock.Clock
 import morphir.cli._
 import morphir.runtime._
 import caseapp.core.RemainingArgs
+import morphir.gateway.ZioGateway.GatewayClient
+import scalapb.zio_grpc.`package`.ZManagedChannel
+import io.grpc.ManagedChannelBuilder
+import morphir.gateway.AboutRequest
+import org.scalacheck.util.CmdLineParser
 
 object AboutCommand extends MorphirCommand[CliCommand.About] {
   def run(commandLine: CliCommand.About, remainingArgs: RemainingArgs): Cmdlet =
-    for {
+    (for {
       _ <- console.putStrLn("Running about command.")
-    } yield ExitCode.Success
+      exitCode <- logic
+    } yield exitCode)
+
+  val clientLayer: ZLayer.NoDeps[Throwable, GatewayClient] = GatewayClient.live(
+    ZManagedChannel(
+      ManagedChannelBuilder.forAddress("localhost", 7779).usePlaintext()
+    )
+  )
+
+  val logic: Cmdlet =
+    (for {
+      _ <- putStrLn(s"Starting client...")
+      r <- GatewayClient.about(AboutRequest()).provideLayer(clientLayer)
+      _ <- putStrLn(s"$r")
+    } yield ExitCode.Success)
+      .onError { c => putStrLn(c.prettyPrint) }
+      .fold(_ => ExitCode.Failure, c => c)
 }
