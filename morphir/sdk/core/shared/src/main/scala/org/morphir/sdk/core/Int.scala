15a1958e19d2087851c09b06d20ed92f0b8fb3e4
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-04-14 18:45:21 -0400

    Expand API functionality

---
+++ morphir/sdk/core/shared/src/main/scala/org/morphir/sdk/core/Int.scala
@@ -0,0 +1,10 @@
+package org.morphir.sdk.core
+
+object Int {
+  type Int = scala.BigInt
+  type Int8 = scala.Byte
+  type Int16 = scala.Short
+  type Int32 = scala.Int
+  type Int64 = scala.Long
+
+}
--- morphir/sdk/core/shared/src/main/scala/org/morphir/sdk/core/String.scala
+++ morphir/sdk/core/shared/src/main/scala/org/morphir/sdk/core/String.scala
@@ -1,6 +1,8 @@
 package org.morphir.sdk.core
 
 object String {
+  type String = scala.Predef.String
+
   @inline def fromInt(int: Int): String =
     int.toString()
 }
