afa15247d33abdd4e64305c104f42fa5b63599cf
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-05-20 08:16:10 -0400

    Add more to the String module

--- morphir/sdk/core/src/org/morphir/sdk/String.scala
+++ morphir/sdk/core/src/org/morphir/sdk/String.scala
@@ -3,11 +3,42 @@ package org.morphir.sdk
 import org.morphir.sdk.Maybe.Maybe
 
 object String {
-  type String = scala.Predef.String
+  type String = ScalaString
+
+  @inline def isEmpty(str: String): Boolean = str.isEmpty()
+
+  @inline def length(str: String): Int = str.length()
+
+  @inline def reverse(str: String): String = str.reverse
+
+  def repeat(times: Int, str: String): String = Array.fill[String](times)(str).mkString
+
+  @inline def replace(literal: String, replacement: String, target: String): String =
+    target.replaceAllLiterally(literal, replacement)
 
   @inline def fromInt(int: Int): String =
     int.toString
 
+  @inline def append(first: String, second: String): String = first + second
+
+  @inline def ++(first: String, second: String): String = first + second
+
+  @inline def concat(strings: List[String]): String = strings.mkString
+
+  @inline def split(sep: String, target: String): String = target.split(sep) //TODO: These aren't exactly the same
+
   @inline def toInt(text: String): Maybe[Int] =
     StringOps.toInt(text)
+
+  @inline def toUpper(text: String): String =
+    text.toUpperCase()
+
+  @inline def toLower(text: String): String =
+    text.toLowerCase()
+
+  @inline def trim(text: String): String = text.trim()
+
+  implicit class StringOps(val self: String) extends AnyVal {
+    def ++(that: String): String = self + that
+  }
 }
--- morphir/sdk/core/src/org/morphir/sdk/package.scala
+++ morphir/sdk/core/src/org/morphir/sdk/package.scala
@@ -1,3 +1,6 @@
 package org.morphir
 
-package object sdk {}
+package object sdk {
+  val StringModule: StringModule = org.morphir.sdk.String
+  type StringModule = org.morphir.sdk.String.type
+}
