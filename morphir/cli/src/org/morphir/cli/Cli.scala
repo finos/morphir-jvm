a29a27dec24ff3e8a0f3727e2ea2ec2c71b031ef
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-05-22 17:29:48 -0400

    Introduce the scala CLI

--- morphir/cli/src/org/morphir/cli/Cli.scala
+++ morphir/cli/src/org/morphir/cli/Cli.scala
@@ -1,7 +1,7 @@
 package org.morphir.cli
 
 import com.monovore.decline._
-import org.morphir.cli.commands.{ CliCommand, ElmCompileCommand, ElmMakeCommand, HelpCommand }
+import org.morphir.cli.commands.{ CliCommand, ElmCompileCommand, ElmMakeCommand, HelpCommand, ScalaGenCommand }
 import zio._
 
 object Cli {
@@ -10,12 +10,16 @@ object Cli {
     ZIO.fromEither(Cli.rootCommand.parse(args)).fold((help: Help) => HelpCommand(help), identity)
 
   lazy val rootCommand: Command[CliCommand] = Command("morphir", "Morphir CLI")(
-    elmCommand
+    elmCommand orElse scalaCommand
   )
 
   lazy val elmCommand: Opts[CliCommand] =
     Opts.subcommand("elm", help = "Access Elm tooling")(
       ElmMakeCommand.Cli.command orElse ElmCompileCommand.Cli.command
     )
+
+  lazy val scalaCommand: Opts[CliCommand] = Opts.subcommand("scala", help = "Access Scala related tooling")(
+    ScalaGenCommand.Cli.generateCommand orElse ScalaGenCommand.Cli.genCommand
+  )
 
 }
