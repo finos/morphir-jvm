1fa68dfa2a23870a131e78ec9645ddb342bd5604
JBarrios <javier.barrios@endava.com>
2020-07-22 11:32:19 -0300

    Books and Records change base directory

--- examples/workspaces/booksandrecords-spring-boot/src/main/java/com/ms/booksandrecords/processor/CmdProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.ms.booksandrecords.processor;
-
-import com.ms.booksandrecords.repository.DealRepository;
-import com.ms.booksandrecords.service.DealEvent;
-
-public interface CmdProcessor <DealCmd>{
-    DealEvent processCmd(DealCmd dealCmd, DealRepository dealRepository);
-}
