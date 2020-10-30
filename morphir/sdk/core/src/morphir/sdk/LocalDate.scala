5555e997c2a2fb7dbffdd67fdfa798c948053834
Stephen Goldbaum <stephen.goldbaum@gmail.com>
2020-10-30 11:14:43 -0400

    Updated return types to reflect SDK

--- morphir/sdk/core/src/morphir/sdk/LocalDate.scala
+++ morphir/sdk/core/src/morphir/sdk/LocalDate.scala
@@ -18,32 +18,11 @@ package morphir.sdk
 
 import java.time.temporal.ChronoUnit._
 
+import morphir.sdk.Int.Int64
+
 object LocalDate {
   type LocalDate = java.time.LocalDate
 
-  type Month = java.time.Month
-
-  val Jan: Month = java.time.Month.JANUARY
-  val Feb: Month = java.time.Month.FEBRUARY
-  val Mar: Month = java.time.Month.MARCH
-  val Apr: Month = java.time.Month.APRIL
-  val May: Month = java.time.Month.MAY
-  val Jun: Month = java.time.Month.JUNE
-  val Jul: Month = java.time.Month.JULY
-  val Aug: Month = java.time.Month.AUGUST
-  val Sep: Month = java.time.Month.SEPTEMBER
-  val Oct: Month = java.time.Month.OCTOBER
-  val Nov: Month = java.time.Month.NOVEMBER
-  val Dec: Month = java.time.Month.DECEMBER
-
-  type DayOfWeek = java.time.DayOfWeek
-  val Sunday: DayOfWeek = java.time.DayOfWeek.SUNDAY
-  val Monday: DayOfWeek = java.time.DayOfWeek.MONDAY
-  val Tuesday: DayOfWeek = java.time.DayOfWeek.TUESDAY
-  val Wednesday: DayOfWeek = java.time.DayOfWeek.WEDNESDAY
-  val Thursday: DayOfWeek = java.time.DayOfWeek.THURSDAY
-  val Friday: DayOfWeek = java.time.DayOfWeek.FRIDAY
-  val Saturday: DayOfWeek = java.time.DayOfWeek.SATURDAY
 
   def addDays(count: Long, date: LocalDate): LocalDate =
     date plusDays count
@@ -57,16 +36,16 @@ object LocalDate {
   def addYears(count: Long, date: LocalDate): LocalDate =
     date plusYears count
 
-  def diffInDays(fromDate: LocalDate, toDate: LocalDate): Long =
+  def diffInDays(fromDate: LocalDate, toDate: LocalDate): Int64 =
     DAYS between (fromDate, toDate)
 
-  def diffInWeeks(fromDate: LocalDate, toDate: LocalDate): Long =
+  def diffInWeeks(fromDate: LocalDate, toDate: LocalDate): Int64 =
     WEEKS between (fromDate, toDate)
 
-  def diffInMonths(fromDate: LocalDate, toDate: LocalDate): Long =
+  def diffInMonths(fromDate: LocalDate, toDate: LocalDate): Int64 =
     MONTHS between (fromDate, toDate)
 
-  def diffInYears(fromDate: LocalDate, toDate: LocalDate): Long =
+  def diffInYears(fromDate: LocalDate, toDate: LocalDate): Int64  =
     YEARS between (fromDate, toDate)
 
   /**
