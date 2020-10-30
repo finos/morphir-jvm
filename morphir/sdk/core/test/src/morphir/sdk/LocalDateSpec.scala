39625f220d50cf3e115fa8ec7948f864c9232e42
Stephen Goldbaum <stephen.goldbaum@gmail.com>
2020-10-30 10:27:47 -0400

    Full LocalDate diff and add function testing

--- morphir/sdk/core/test/src/morphir/sdk/LocalDateSpec.scala
+++ morphir/sdk/core/test/src/morphir/sdk/LocalDateSpec.scala
@@ -26,49 +26,92 @@ object LocalDateSpec extends DefaultRunnableSpec {
   def spec = suite("Addition") {
     suite("Add days") {
       test("Identity: Adding 0") {
-
         assert(SdkDate.addDays(0, date))(equalTo(date.plusDays(0)))
       }
-//      test("Identity: Adding up") {
-//        assert(SdkDate.addDays(date, 1)(equalTo(date.plusDays(1))))
-//      }
-//      test("Identity: Adding down") {
-//        assert(SdkDate.addDays(date, -1)(equalTo(date.plusDays(-1))))
-//      }
+      test("Identity: Adding up") {
+        assert(SdkDate.addDays(1, date))(equalTo(date.plusDays(1)))
+      }
+      test("Identity: Adding down") {
+        assert(SdkDate.addDays(-1, date))(equalTo(date.plusDays(-1)))
+      }
     }
-//    suite("Add weeks") {
-//      test("Identity: Adding 0") {
-//        assert(SdkDate.addWeeks(date, 0)(equalTo(date.plusWeeks(0))))
-//      }
-//      test("Identity: Adding up") {
-//        assert(SdkDate.addWeeks(date, 1)(equalTo(date.plusWeeks(1))))
-//      }
-//      test("Identity: Adding down") {
-//        assert(SdkDate.addWeeks(date, -1)(equalTo(date.plusWeeks(-1))))
-//      }
-//    };
-//    suite("Add months") {
-//      test("Identity: Adding 0") {
-//        assert(SdkDate.addMonths(date, 0)(equalTo(date.plusMonths(0))))
-//      }
-//      test("Identity: Adding up") {
-//        assert(SdkDate.addMonths(date, 1)(equalTo(date.plusMonths(1))))
-//      }
-//      test("Identity: Adding down") {
-//        assert(SdkDate.addMonths(date, -1)(equalTo(date.plusMonths(-1))))
-//      }
-//    };
-//    suite("Add years") {
-//      test("Identity: Adding 0") {
-//        assert(SdkDate.addYears(date, 0)(equalTo(date.plusYears(0))))
-//      }
-//      test("Identity: Adding up") {
-//        assert(SdkDate.addYears(date, 1)(equalTo(date.plusYears(1))))
-//      }
-//      test("Identity: Adding down") {
-//        assert(SdkDate.addYears(date, -1)(equalTo(date.plusYears(-1))))
-//      }
-//    }
+    suite("Add weeks") {
+      test("Identity: Adding 0") {
+        assert(SdkDate.addWeeks(0, date))(equalTo(date.plusWeeks(0)))
+      }
+      test("Identity: Adding up") {
+        assert(SdkDate.addWeeks(1, date))(equalTo(date.plusWeeks(1)))
+      }
+      test("Identity: Adding down") {
+        assert(SdkDate.addWeeks(-1, date))(equalTo(date.plusWeeks(-1)))
+      }
+    }
+    suite("Add months") {
+      test("Identity: Adding 0") {
+        assert(SdkDate.addMonths(0, date))(equalTo(date.plusMonths(0)))
+      }
+      test("Identity: Adding up") {
+        assert(SdkDate.addMonths(1, date))(equalTo(date.plusMonths(1)))
+      }
+      test("Identity: Adding down") {
+        assert(SdkDate.addMonths(-1, date))(equalTo(date.plusMonths(-1)))
+      }
+    }
+    suite("Add years") {
+      test("Identity: Adding 0") {
+        assert(SdkDate.addYears(0, date))(equalTo(date.plusYears(0)))
+      }
+      test("Identity: Adding up") {
+        assert(SdkDate.addYears(1, date))(equalTo(date.plusYears(1)))
+      }
+      test("Identity: Adding down") {
+        assert(SdkDate.addYears(-1, date))(equalTo(date.plusYears(-1)))
+      }
+    }
+    suite("Diff days") {
+      test("Identity: diff self") {
+        assert(SdkDate.diffInDays(date, date))(equalTo(0L))
+      }
+      test("Identity: diff later") {
+        assert(SdkDate.diffInDays(date, date.plusDays(1)))(equalTo(1L))
+      }
+      test("Identity: diff earlier") {
+        assert(SdkDate.diffInDays(date, date.plusDays(-1)))(equalTo(-1L))
+      }
+    }
+    suite("Diff weeks") {
+      test("Identity: diff self") {
+        assert(SdkDate.diffInWeeks(date, date))(equalTo(0L))
+      }
+      test("Identity: diff later") {
+        assert(SdkDate.diffInWeeks(date, date.plusWeeks(1)))(equalTo(1L))
+      }
+      test("Identity: diff earlier") {
+        assert(SdkDate.diffInWeeks(date, date.plusWeeks(-1)))(equalTo(-1L))
+      }
+    }
+    suite("Diff days") {
+      test("Identity: diff self") {
+        assert(SdkDate.diffInMonths(date, date))(equalTo(0L))
+      }
+      test("Identity: diff later") {
+        assert(SdkDate.diffInMonths(date, date.plusMonths(1)))(equalTo(1L))
+      }
+      test("Identity: diff earlier") {
+        assert(SdkDate.diffInMonths(date, date.plusMonths(-1)))(equalTo(-1L))
+      }
+    }
+    suite("Diff days") {
+      test("Identity: diff self") {
+        assert(SdkDate.diffInYears(date, date))(equalTo(0L))
+      }
+      test("Identity: diff later") {
+        assert(SdkDate.diffInYears(date, date.plusYears(1)))(equalTo(1L))
+      }
+      test("Identity: diff earlier") {
+        assert(SdkDate.diffInYears(date, date.plusYears(-1)))(equalTo(-1L))
+      }
+    }
   }
 
   case class Wrapped[A](value: A)
