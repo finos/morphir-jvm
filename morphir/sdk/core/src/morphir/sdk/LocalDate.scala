01ec9cdf9fc1581b435852904888b808de3c744d
Stephen Goldbaum <129341+stephengoldbaum@users.noreply.github.com>
2020-10-30 11:16:34 -0400

    Added LocalDate diff and add support (#64)
    * Added LocalDate functions and tests
    
    * Fix LocalDate test
    
    * Full LocalDate diff and add function testing
    
    * Added return types
    
    * Updated return types to reflect SDK
    
    Co-authored-by: Damian Reeves <957246+DamianReeves@users.noreply.github.com>

--- morphir/sdk/core/src/morphir/sdk/LocalDate.scala
+++ morphir/sdk/core/src/morphir/sdk/LocalDate.scala
@@ -12,36 +12,47 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
-
+ */
 
 package morphir.sdk
+
+import java.time.temporal.ChronoUnit._
+
+import morphir.sdk.Int.Int64
 
 object LocalDate {
   type LocalDate = java.time.LocalDate
 
-  type Month = java.time.Month
 
-  val Jan: Month = java.time.Month.JANUARY
-  val Feb: Month = java.time.Month.FEBRUARY
-  val Mar: Month = java.time.Month.MARCH
-  val Apr: Month = java.time.Month.APRIL
-  val May: Month = java.time.Month.MAY
-  val Jun: Month = java.time.Month.JUNE
-  val Jul: Month = java.time.Month.JULY
-  val Aug: Month = java.time.Month.AUGUST
-  val Sep: Month = java.time.Month.SEPTEMBER
-  val Oct: Month = java.time.Month.OCTOBER
-  val Nov: Month = java.time.Month.NOVEMBER
-  val Dec: Month = java.time.Month.DECEMBER
+  def addDays(count: Long, date: LocalDate): LocalDate =
+    date plusDays count
 
-  type DayOfWeek = java.time.DayOfWeek
-  val Sunday: DayOfWeek    = java.time.DayOfWeek.SUNDAY
-  val Monday: DayOfWeek    = java.time.DayOfWeek.MONDAY
-  val Tuesday: DayOfWeek   = java.time.DayOfWeek.TUESDAY
-  val Wednesday: DayOfWeek = java.time.DayOfWeek.WEDNESDAY
-  val Thursday: DayOfWeek  = java.time.DayOfWeek.THURSDAY
-  val Friday: DayOfWeek    = java.time.DayOfWeek.FRIDAY
-  val Saturday: DayOfWeek  = java.time.DayOfWeek.SATURDAY
+  def addWeeks(count: Long, date: LocalDate): LocalDate =
+    date plusWeeks count
 
+  def addMonths(count: Long, date: LocalDate): LocalDate =
+    date plusMonths count
+
+  def addYears(count: Long, date: LocalDate): LocalDate =
+    date plusYears count
+
+  def diffInDays(fromDate: LocalDate, toDate: LocalDate): Int64 =
+    DAYS between (fromDate, toDate)
+
+  def diffInWeeks(fromDate: LocalDate, toDate: LocalDate): Int64 =
+    WEEKS between (fromDate, toDate)
+
+  def diffInMonths(fromDate: LocalDate, toDate: LocalDate): Int64 =
+    MONTHS between (fromDate, toDate)
+
+  def diffInYears(fromDate: LocalDate, toDate: LocalDate): Int64  =
+    YEARS between (fromDate, toDate)
+
+  /**
+    * Provides a conversion from a `java.time.LocalDate` to a `morphir.sdk.LocalDate.LocalDate`
+    */
+  implicit def fromJavaTimeLocalDate(
+      localDate: java.time.LocalDate
+  ): LocalDate =
+    localDate
 }
---
+++ morphir/sdk/core/test/src/morphir/sdk/LocalDateSpec.scala
@@ -0,0 +1,118 @@
+/*
+Copyright 2020 Morgan Stanley
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package morphir.sdk
+
+import zio.test._
+import zio.test.Assertion._
+import morphir.sdk.{LocalDate => SdkDate}
+
+object LocalDateSpec extends DefaultRunnableSpec {
+  val date = java.time.LocalDate.now
+
+  def spec = suite("Addition") {
+    suite("Add days") {
+      test("Identity: Adding 0") {
+        assert(SdkDate.addDays(0, date))(equalTo(date.plusDays(0)))
+      }
+      test("Identity: Adding up") {
+        assert(SdkDate.addDays(1, date))(equalTo(date.plusDays(1)))
+      }
+      test("Identity: Adding down") {
+        assert(SdkDate.addDays(-1, date))(equalTo(date.plusDays(-1)))
+      }
+    }
+    suite("Add weeks") {
+      test("Identity: Adding 0") {
+        assert(SdkDate.addWeeks(0, date))(equalTo(date.plusWeeks(0)))
+      }
+      test("Identity: Adding up") {
+        assert(SdkDate.addWeeks(1, date))(equalTo(date.plusWeeks(1)))
+      }
+      test("Identity: Adding down") {
+        assert(SdkDate.addWeeks(-1, date))(equalTo(date.plusWeeks(-1)))
+      }
+    }
+    suite("Add months") {
+      test("Identity: Adding 0") {
+        assert(SdkDate.addMonths(0, date))(equalTo(date.plusMonths(0)))
+      }
+      test("Identity: Adding up") {
+        assert(SdkDate.addMonths(1, date))(equalTo(date.plusMonths(1)))
+      }
+      test("Identity: Adding down") {
+        assert(SdkDate.addMonths(-1, date))(equalTo(date.plusMonths(-1)))
+      }
+    }
+    suite("Add years") {
+      test("Identity: Adding 0") {
+        assert(SdkDate.addYears(0, date))(equalTo(date.plusYears(0)))
+      }
+      test("Identity: Adding up") {
+        assert(SdkDate.addYears(1, date))(equalTo(date.plusYears(1)))
+      }
+      test("Identity: Adding down") {
+        assert(SdkDate.addYears(-1, date))(equalTo(date.plusYears(-1)))
+      }
+    }
+    suite("Diff days") {
+      test("Identity: diff self") {
+        assert(SdkDate.diffInDays(date, date))(equalTo(0L))
+      }
+      test("Identity: diff later") {
+        assert(SdkDate.diffInDays(date, date.plusDays(1)))(equalTo(1L))
+      }
+      test("Identity: diff earlier") {
+        assert(SdkDate.diffInDays(date, date.plusDays(-1)))(equalTo(-1L))
+      }
+    }
+    suite("Diff weeks") {
+      test("Identity: diff self") {
+        assert(SdkDate.diffInWeeks(date, date))(equalTo(0L))
+      }
+      test("Identity: diff later") {
+        assert(SdkDate.diffInWeeks(date, date.plusWeeks(1)))(equalTo(1L))
+      }
+      test("Identity: diff earlier") {
+        assert(SdkDate.diffInWeeks(date, date.plusWeeks(-1)))(equalTo(-1L))
+      }
+    }
+    suite("Diff days") {
+      test("Identity: diff self") {
+        assert(SdkDate.diffInMonths(date, date))(equalTo(0L))
+      }
+      test("Identity: diff later") {
+        assert(SdkDate.diffInMonths(date, date.plusMonths(1)))(equalTo(1L))
+      }
+      test("Identity: diff earlier") {
+        assert(SdkDate.diffInMonths(date, date.plusMonths(-1)))(equalTo(-1L))
+      }
+    }
+    suite("Diff days") {
+      test("Identity: diff self") {
+        assert(SdkDate.diffInYears(date, date))(equalTo(0L))
+      }
+      test("Identity: diff later") {
+        assert(SdkDate.diffInYears(date, date.plusYears(1)))(equalTo(1L))
+      }
+      test("Identity: diff earlier") {
+        assert(SdkDate.diffInYears(date, date.plusYears(-1)))(equalTo(-1L))
+      }
+    }
+  }
+
+  case class Wrapped[A](value: A)
+}
