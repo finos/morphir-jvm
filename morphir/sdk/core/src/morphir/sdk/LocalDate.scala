f33c546f0150dd363acac7b99a3b995178bb9030
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-10-30 09:30:27 -0400

    Fix LocalDate test

--- morphir/sdk/core/src/morphir/sdk/LocalDate.scala
+++ morphir/sdk/core/src/morphir/sdk/LocalDate.scala
@@ -12,8 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
-
+ */
 
 package morphir.sdk
 
@@ -38,37 +37,43 @@ object LocalDate {
   val Dec: Month = java.time.Month.DECEMBER
 
   type DayOfWeek = java.time.DayOfWeek
-  val Sunday: DayOfWeek    = java.time.DayOfWeek.SUNDAY
-  val Monday: DayOfWeek    = java.time.DayOfWeek.MONDAY
-  val Tuesday: DayOfWeek   = java.time.DayOfWeek.TUESDAY
+  val Sunday: DayOfWeek = java.time.DayOfWeek.SUNDAY
+  val Monday: DayOfWeek = java.time.DayOfWeek.MONDAY
+  val Tuesday: DayOfWeek = java.time.DayOfWeek.TUESDAY
   val Wednesday: DayOfWeek = java.time.DayOfWeek.WEDNESDAY
-  val Thursday: DayOfWeek  = java.time.DayOfWeek.THURSDAY
-  val Friday: DayOfWeek    = java.time.DayOfWeek.FRIDAY
-  val Saturday: DayOfWeek  = java.time.DayOfWeek.SATURDAY
+  val Thursday: DayOfWeek = java.time.DayOfWeek.THURSDAY
+  val Friday: DayOfWeek = java.time.DayOfWeek.FRIDAY
+  val Saturday: DayOfWeek = java.time.DayOfWeek.SATURDAY
 
-
-  def diffInDays(fromDate : LocalDate, toDate : LocalDate) =
-    DAYS.between(fromDate, toDate)
-
-  def diffInWeeks(fromDate : LocalDate, toDate : LocalDate) =
-    WEEKS.between(fromDate, toDate)
-
-  def diffInMonths(fromDate : LocalDate, toDate : LocalDate) =
-    MONTHS.between(fromDate, toDate)
-
-  def diffInYears(fromDate : LocalDate, toDate : LocalDate) =
-    YEARS.between(fromDate, toDate)
-
-  def addDays (count: Long, date : LocalDate) =
+  def addDays(count: Long, date: LocalDate) =
     date plusDays count
 
-  def addWeeks(count: Long, date : LocalDate) =
+  def addWeeks(count: Long, date: LocalDate) =
     date plusWeeks count
 
-  def addMonths (count: Long, date : LocalDate) =
+  def addMonths(count: Long, date: LocalDate) =
     date plusMonths count
 
-  def addYears (count: Long, date : LocalDate) =
+  def addYears(count: Long, date: LocalDate) =
     date plusYears count
 
+  def diffInDays(fromDate: LocalDate, toDate: LocalDate) =
+    DAYS.between(fromDate, toDate)
+
+  def diffInWeeks(fromDate: LocalDate, toDate: LocalDate) =
+    WEEKS.between(fromDate, toDate)
+
+  def diffInMonths(fromDate: LocalDate, toDate: LocalDate) =
+    MONTHS.between(fromDate, toDate)
+
+  def diffInYears(fromDate: LocalDate, toDate: LocalDate) =
+    YEARS.between(fromDate, toDate)
+
+  /**
+    * Provides a conversion from a `java.time.LocalDate` to a `morphir.sdk.LocalDate.LocalDate`
+    */
+  implicit def fromJavaTimeLocalDate(
+      localDate: java.time.LocalDate
+  ): LocalDate =
+    localDate
 }
