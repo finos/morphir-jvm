572df97748fb450507ce2de48c02aaea2f753b70
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-12-03 10:54:32 -0500

    Use Warn level logging for spark

--- morphir/flowz/spark/test/src/morphir/flowz/spark/SparkModuleSpec.scala
+++ morphir/flowz/spark/test/src/morphir/flowz/spark/SparkModuleSpec.scala
@@ -1,9 +1,9 @@
 package morphir.flowz.spark
 
 import morphir.flowz.spark.sparkModule.SparkModule
+import morphir.flowz.spark.sample.model._
 import morphir.flowz.spark.testing.SparkSpec
 import zio.ZIO
-import zio.random.Random
 import zio.test._
 import zio.test.Assertion._
 
@@ -60,34 +60,4 @@ object SparkModuleSpec extends SparkSpec {
     )
   )
 
-  final case class FirstName(value: String)
-
-  object FirstName {
-    val gen: Gen[Any, FirstName] =
-      Gen
-        .fromIterable(Seq("Albert", "Bobby", "Carl", "Claude", "David", "George", "Thomas", "William", "Xavier"))
-        .map(FirstName(_))
-  }
-
-  final case class LastName(value: String)
-  object LastName {
-    val gen: Gen[Any, LastName] =
-      Gen
-        .fromIterable(
-          Seq("Alexander", "Black", "Brown", "James", "Jefferson", "Jones", "Smith", "Washington", "Williams")
-        )
-        .map(LastName(_))
-  }
-
-  final case class Person(firstName: FirstName, lastName: LastName)
-  object Person {
-    val gen: Gen[Any, Person] =
-      for {
-        firstName <- FirstName.gen
-        lastName  <- LastName.gen
-      } yield Person(firstName, lastName)
-
-    def genPeople(min: Int, max: Int): Gen[Random with Sized, Set[Person]] =
-      Gen.setOfBounded(min, max)(gen)
-  }
 }
