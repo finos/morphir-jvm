ab1c0122209909a621aafecc53cc5d33cf011e7f
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-12-08 20:37:10 +0000

    Fix FiberSyntax

--- morphir/flowz/src/morphir/flowz/FiberSyntax.scala
+++ morphir/flowz/src/morphir/flowz/FiberSyntax.scala
@@ -2,7 +2,6 @@ package morphir.flowz
 
 import morphir.flowz.FiberSyntax.FiberOutputChannelOps
 import zio._
-import scala.language.implicitConversions
 
 trait FiberSyntax {
   implicit def toFiberOutputChannelOps[State, Err, Output](
@@ -14,6 +13,6 @@ trait FiberSyntax {
 
 object FiberSyntax {
   class FiberOutputChannelOps[+State, +Err, +Output](val self: Fiber[Err, OutputChannels[State, Err]]) extends {
-    def joinFlow = Flow.fromEffect(self.join)
+    def joinFlow: SrcFlow[State, Err, Err] = Flow.join(self)
   }
 }
