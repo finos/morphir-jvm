59ed34bcd39c549f1161752ffad36f4b71783a90
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-12-09 13:52:21 -0500

    Fix compilation issue

--- morphir/flowz/spark/test/src/morphir/flowz/spark/sample/heroes/HeroesSampleMain.scala
+++ morphir/flowz/spark/test/src/morphir/flowz/spark/sample/heroes/HeroesSampleMain.scala
@@ -123,7 +123,7 @@ object HeroesSampleMain extends App {
     : SparkFlow[DataSources, DataSources, Any, Dataset[String], Throwable, Dataset[HeroAbilities]] =
     Step.context[Any, DataSources, Dataset[String]].flatMap { ctx =>
       val heroAbilities = ctx.inputs.state.heroAbilities
-      val abilities     = ctx.inputs.state.abil
+      val abilities     = ctx.inputs.state.abilities
       SparkStep.withSpark { spark =>
         implicit val sqlContext: SQLContext = spark.sqlContext
         import sqlContext.implicits._
