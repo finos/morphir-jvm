70923a5a6bedb6533f144c06fdc38f4c657eda3a
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2021-06-30 11:50:17 -0400

    Adding Package related IR types

--- ir/src/main/scala/org/finos/morphir/ir/PackageName.scala
+++ ir/src/main/scala/org/finos/morphir/ir/PackageName.scala
@@ -1,3 +1,4 @@
 package org.finos.morphir.ir
 
 final case class PackageName(path:List[Name])
+
---
+++ ir/src/test/scala/org/finos/morphir/ir/PackageSpecificationSpec.scala
@@ -0,0 +1,9 @@
+package org.finos.morphir.ir
+import zio.test.{assert, DefaultRunnableSpec}
+import zio.test.Assertion.*
+
+object PackageSpecificationSpec extends DefaultRunnableSpec {
+  def spec = suite("PackageSpecification spec")()
+}
+
+
