d40849547272bf56923505ce6a4d4d52b4948fb8
Attila Mihaly <60483498+AttilaMihaly@users.noreply.github.com>
2021-07-20 15:44:47 -0400

    Fixed bug. #99 (#100)

--- morphir/sdk/core/src/morphir/sdk/Decimal.scala
+++ morphir/sdk/core/src/morphir/sdk/Decimal.scala
@@ -1,8 +1,9 @@
 package morphir.sdk
-import morphir.sdk.Maybe.Maybe
-import morphir.sdk.Basics.Order
-
 import java.math.{ BigDecimal => BigDec }
+
+import morphir.sdk.Basics.Order
+import morphir.sdk.Maybe.Maybe
+
 import scala.util.control.NonFatal
 
 object Decimal extends DecimalModuleCompat {
@@ -78,7 +79,7 @@ object Decimal extends DecimalModuleCompat {
     Decimal(n * 0.01)
 
   def gt(a: Decimal)(b: Decimal): morphir.sdk.Bool.Bool  = a.compareTo(b) > 0
-  def gte(a: Decimal)(b: Decimal): morphir.sdk.Bool.Bool = a.compareTo(b) >= 1
+  def gte(a: Decimal)(b: Decimal): morphir.sdk.Bool.Bool = a.compareTo(b) >= 0
 
   def lt(a: Decimal)(b: Decimal): morphir.sdk.Bool.Bool = a.compareTo(b) < 0
 
