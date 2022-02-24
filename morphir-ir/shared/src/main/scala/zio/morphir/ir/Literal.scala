5a13f464a21abc822446a2870b21d3ede68a2644
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-02-24 15:23:35 -0500

    Create a Literal Value from a Literal

--- morphir-ir/shared/src/main/scala/zio/morphir/ir/Literal.scala
+++ morphir-ir/shared/src/main/scala/zio/morphir/ir/Literal.scala
@@ -1,7 +1,11 @@
 package zio.morphir.ir
+import scala.language.implicitConversions
+import zio.morphir.ir.ValueModule.Value
+import zio.ZEnvironment
 
-sealed trait Literal[+A] {
+sealed trait Literal[+A] { self =>
   def value: A
+  def toIRValue: Value.Literal[A, Any] = Value.Literal(self, ZEnvironment.empty)
 }
 object Literal {
   def boolean(value: Boolean): Bool                         = Bool(value)
@@ -10,9 +14,12 @@ object Literal {
   def string(value: java.lang.String): String               = Literal.String(value)
   def wholeNumber(value: java.math.BigInteger): WholeNumber = WholeNumber(value)
 
+  implicit def literalToIRValue[A](literal: Literal[A]): Value.Literal[A, Any] = literal.toIRValue
+
   final case class Bool(value: scala.Boolean)               extends Literal[scala.Boolean]
   final case class Char(value: scala.Char)                  extends Literal[scala.Char]
   final case class String(value: java.lang.String)          extends Literal[java.lang.String]
   final case class WholeNumber(value: java.math.BigInteger) extends Literal[java.math.BigInteger]
   final case class Float(value: java.math.BigDecimal)       extends Literal[java.math.BigDecimal]
+
 }
