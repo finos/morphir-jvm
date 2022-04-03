b6ef5b3dd49dbdf883a37a0ee665bdccbbcce4c4
Michelle <michelchan421@gmail.com>
2022-04-03 12:27:16 -0400

    Adding to TypeModule (#85)
    * adding to TypeModule
    
    * removing unused import

--- morphir-ir/shared/src/main/scala/zio/morphir/ir/types/TypeModule.scala
+++ morphir-ir/shared/src/main/scala/zio/morphir/ir/types/TypeModule.scala
@@ -1,5 +1,8 @@
 package zio.morphir.ir.types
 
+import zio.morphir.ir.{FQName, Name}
+//import zio.morphir.sdk.ResultModule.Result
+
 trait TypeModule extends TypeModuleSyntax {
   final type Type[+A] = zio.morphir.ir.types.Type[A]
   val Type: zio.morphir.ir.types.Type.type = zio.morphir.ir.types.Type
@@ -22,8 +25,26 @@ trait TypeModule extends TypeModuleSyntax {
   final def mapSpecificationAttributes[A, B](f: A => B, spec: Specification[A]): Specification[B] = ???
 //    spec.map(f)
 
-//  def mapDefinitionAttributes[A, B] (f: A => B, defn: Definition[A]) : Definition[B] = defn.map(f)
+//  def mapDefinition[A, B, E](
+//      f: Type[A] => Result[E, Type[B]],
+//      definition: Definition[A]
+//  ): Result[List[E], Definition[B]] =
+//    definition.map(f)
 
+  def mapDefinitionAttributes[A, B](f: A => B, defn: Definition[A]): Definition[B] = ??? // defn.map(f)
+
+  def mapTypeAttributes[A, B](f: A => B, tpe: Type[A]): Type[B] = tpe.mapAttributes(f)
+
+  def typeAttributes[A](tpe: Type[A]): A = tpe.attributes
+
+  def eraseAttributes[A](typeDef: Definition[A]): UDefinition = ??? // typeDef.eraseAttributes
+
+  def collectVariables[A](tpe: Type[A]): Set[Name]    = tpe.collectVariables
+  def collectReferences[A](tpe: Type[A]): Set[FQName] = tpe.collectReferences
+  def substituteTypeVariables[A](mapping: Map[Name, Type[A]], tpe: Type[A]): Type[A] =
+    ??? // tpe.substituteTypeVariables
+
+  def toString[A](tpe: Type[A]): String = tpe.toString
 }
 
 final object TypeModule extends TypeModule
