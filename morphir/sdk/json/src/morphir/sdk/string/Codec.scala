87292c1ec112fde30ee61cdfd5a15051c5eb686d
Osborne Yaw Sapaty <osborne.sapaty@morganstanley.com>
2022-04-25 12:01:12 +0000

    codec implemented for list, tuples & basics

--- morphir/sdk/json/src/morphir/sdk/string/Codec.scala
+++ morphir/sdk/json/src/morphir/sdk/string/Codec.scala
@@ -4,11 +4,10 @@ import io.circe.{ Decoder, Encoder }
 import morphir.sdk.String.String
 
 object Codec {
-
+  /* Encoder / Decoder For String Types*/
   implicit val decodeString: Decoder[String] =
     Decoder.decodeString
 
   implicit val encodeString: Encoder[String] =
     Encoder.encodeString
-
 }
