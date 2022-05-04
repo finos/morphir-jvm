b5fcb27e12b3808ca41fd72765b0a45b8ca4aca9
klahnunya <98951378+klahnunya@users.noreply.github.com>
2022-05-04 12:02:42 +0000

    Codec for Result and Maybe (#111)
    * codec for maybe
    
    * codec for result
    
    * format code

---
+++ morphir/sdk/json/src/morphir/sdk/maybe/Codec.scala
@@ -0,0 +1,23 @@
+package morphir.sdk.maybe
+
+import io.circe.{ Decoder, Encoder, HCursor, Json }
+import morphir.sdk.Maybe
+import morphir.sdk.Maybe.Maybe
+
+object Codec {
+  implicit def encodeMaybe[A](implicit encodeA: Encoder[A]): Encoder[Maybe[A]] =
+    (maybeVal: Maybe[A]) =>
+      maybeVal match {
+        case Maybe.Just(value) => encodeA(value)
+        case Maybe.Nothing     => Json.Null
+      }
+
+  implicit def decodeMaybe[A](implicit decodeA: Decoder[A]): Decoder[Maybe[A]] =
+    (c: HCursor) =>
+      c.downN(0).as(decodeA).map { value =>
+        value match {
+          case Json.Null => Maybe.Nothing
+          case va        => Maybe.Just(va)
+        }
+      }
+}
