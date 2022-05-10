0f79394cb41e40de91d8e7a0c28ed260f02726cf
unknown <nunya.klah@morganstanley.com>
2022-05-10 10:06:50 +0000

    val name changes in tests

--- morphir/spark/test/src/ExampleTests.scala
+++ morphir/spark/test/src/ExampleTests.scala
@@ -2,14 +2,16 @@ import org.apache.spark.sql.SparkSession
 import org.scalatest.FunSuite
 
 class test extends FunSuite {
+  val localTestSession = SparkSession.builder().master("local").appName("Example").getOrCreate()
   test("test initializing spark context") {
-    val myLocalInProcessSession = SparkSession.builder().master("local").appName("Example").getOrCreate()
 
     val list = List(1, 2, 3, 4)
-    val rdd  = myLocalInProcessSession.sparkContext.parallelize(list)
+    val rdd  = localTestSession.sparkContext.parallelize(list)
 
     assert(rdd.count === list.length)
-    myLocalInProcessSession.close()
+    localTestSession.close()
 
   }
+  test("test that dataframe is created") {}
+  test("test that filter result is correct") {}
 }
