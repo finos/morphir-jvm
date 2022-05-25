7ac82b5c963540af9d223515d517564ab6a1f2d4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-05-25 18:20:06 +0200

    Update scala-java-time, ... to 2.4.0-M3 (#122)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val `scala-collection-compat` = "2.7.0"
     val scalafix                  = "0.9.34"
     val scalafmt                  = "3.5.0"
-    val `scala-java-time`         = "2.4.0-M2"
+    val `scala-java-time`         = "2.4.0-M3"
     val `zio-cli`                 = "0.2.5"
     val zio                       = "2.0.0-RC5"
     val `zio-json`                = "0.3.0-RC7"
