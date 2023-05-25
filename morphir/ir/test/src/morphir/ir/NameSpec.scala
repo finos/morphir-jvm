3865568933236d1fc7a46dd57e9d70c71973a3ac
Osborne Yaw Sapaty <osborne.sapaty@morganstanley.com>
2023-05-25 15:54:22 +0000

    test for encoding and decoding after codecs change

--- morphir/ir/test/src/morphir/ir/NameSpec.scala
+++ morphir/ir/test/src/morphir/ir/NameSpec.scala
@@ -1,166 +1,28 @@
-/*
-Copyright 2020 Morgan Stanley
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
- */
-
 package morphir.ir
-import zio.test._
+
+import io.circe.{Json, ParsingFailure, parser}
+import morphir.ir.Name.Name
+import morphir.ir.name.Codec
 import zio.test.Assertion._
-import zio.test.TestAspect._
-import morphir.ir.fuzzer.NameFuzzers._
-import morphir.ir.testing.JsonSpec
+import zio.test._
 
-object NameSpec extends DefaultRunnableSpec with JsonSpec {
-  def spec =
-    suite("NameSpec")(
-      suite("Make a Name from a string and check that:")(
-        suite("Name should be creatable from a single word that:")(
-          test("Starts with a capital letter") {
-            assert(Name.fromString("Marco"))(
-              equalTo(Name("marco"))
-            )
-          },
-          test("Is all lowercase") {
-            assert(Name.fromString("polo"))(equalTo(Name("polo")))
-          }
-        ),
-        suite("Name should be creatable from compound words that:")(
-          test("Are formed from a snake case word") {
-            assert(Name.fromString("super_mario_world"))(
-              equalTo(Name("super", "mario", "world"))
-            )
-          },
-          test("Contain many kinds of word delimiters") {
-            assert(Name.fromString("fooBar_baz 123"))(
-              equalTo(Name("foo", "bar", "baz", "123"))
-            )
-          },
-          test("Are formed from a camel-cased string") {
-            assert(Name.fromString("valueInUSD"))(
-              equalTo(Name("value", "in", "u", "s", "d"))
-            )
-          },
-          test("Are formed from a title-cased string") {
+object NameSpec extends DefaultRunnableSpec {
+    val irName: Name = Name.fromString("fooBar_baz 123")
+    val nameInIRJson = """["foo", "bar","baz", "123"]"""
+    val nameFromIRJson: Either[ParsingFailure, Json] =  parser.parse(nameInIRJson)
 
-            assert(Name.fromString("ValueInUSD"))(
-              equalTo(Name("value", "in", "u", "s", "d"))
-            )
-          },
-          test("Are formed from a title-cased string (2)") {
+    override def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] = suite("Name Encoding / Decoding") (
+        test("Encoding Name"){
+            val encodedName = Codec.encodeName(irName)
+            val expectedNameInIRJson = nameFromIRJson.getOrElse(Json.Null)
 
-            assert(Name("ValueInUSD"))(
-              equalTo(Name("value", "in", "u", "s", "d"))
-            )
-          },
-          test("Are have a number in the middle") {
+            assert(encodedName)(equalTo(expectedNameInIRJson))
+        },
+        test("Decoding Name") {
+            val nameFromJson = nameFromIRJson.getOrElse(Json.Null)
+            val decodedName = Codec.decodeName(nameFromJson.hcursor)
 
-            assert(Name.fromString("Nintendo64VideoGameSystem"))(
-              equalTo(Name("nintendo", "64", "video", "game", "system"))
-            )
-          },
-          test("Complete and utter nonsense") {
-            assert(Name.fromString("_-%"))(equalTo(Name("")))
-          }
-        )
-      ),
-      suite("Name should be convertible to a title-case string:")(
-        test(
-          "When the name was originally constructed from a snake-case string"
-        ) {
-          val sut = Name.fromString("snake_case_input")
-          assert(Name.toTitleCase(sut))(equalTo("SnakeCaseInput"))
-        },
-        test(
-          "When the name was originally constructed from a camel-case string"
-        ) {
-          val sut = Name.fromString("camelCaseInput")
-          assert(Name.toTitleCase(sut))(equalTo("CamelCaseInput"))
+            assert(decodedName.getOrElse(Json.Null))(equalTo(irName))
         }
-      ),
-      suite("Name should be convertible to a camel-case string:")(
-        test(
-          "When the name was originally constructed from a snake-case string"
-        ) {
-          val sut = Name.fromString("snake_case_input")
-          assert(Name.toCamelCase(sut))(equalTo("snakeCaseInput"))
-        },
-        test(
-          "When the name was originally constructed from a camel-case string"
-        ) {
-          val sut = Name.fromString("camelCaseInput")
-          assert(Name.toCamelCase(sut))(equalTo("camelCaseInput"))
-        }
-      ),
-      suite("Name should be convertible to snake-case")(
-        test("When given a name constructed from a list of words") {
-          val input = Name.fromList(List("foo", "bar", "baz", "123"))
-          assert(Name.toSnakeCase(input))(equalTo("foo_bar_baz_123"))
-        },
-        test("When the name has parts of an abbreviation") {
-          val name = Name.fromList(List("value", "in", "u", "s", "d"))
-          assert(Name.toSnakeCase(name))(
-            equalTo("value_in_USD")
-          )
-        }
-      ),
-      suite("Name should be convertible to kebab-case")(
-        test("When given a name constructed from a list of words") {
-          val input = Name.fromList(List("foo", "bar", "baz", "123"))
-          assert(Name.toKebabCase(input))(equalTo("foo-bar-baz-123"))
-        },
-        test("When the name has parts of an abbreviation") {
-          val name = Name.fromList(List("value", "in", "u", "s", "d"))
-          assert(Name.toKebabCase(name))(
-            equalTo("value-in-USD")
-          )
-        }
-      ),
-      suite("Name toHumanWords should provide a list of words from a Name")(
-        test("When the name is from a camelCase string") {
-          val sut = Name.fromString("ValueInUSD")
-          assert(Name.toHumanWords(sut))(equalTo(List("value", "in", "USD")))
-        }
-      ),
-      suite("Name encoding/decoding")(
-        testM("Should work in a well-behaved manner") {
-          checkM(fuzzName)(givenName => checkCodecIsWellBehaved(givenName))
-        },
-        testM("A Name should encode as expected") {
-          check(fuzzName)(givenName => assertEncodesToExpectedCompactJsonString(givenName)(givenName.show))
-        },
-        test(
-          """Given a Name whose value is ["delta","sigma","theta"] it should encode correctly"""
-        ) {
-          checkEncodesTo(name("delta", "sigma", "theta"))(
-            ujson.Arr(
-              ujson.Str("delta"),
-              ujson.Str("sigma"),
-              ujson.Str("theta")
-            )
-          )
-        },
-        test(
-          """Given a Name whose value is ["sigma","gamma","ro"] it should encode correctly"""
-        ) {
-          checkEncodesTo(name("sigma", "gamma", "ro"))(
-            ujson.Arr(
-              ujson.Str("sigma"),
-              ujson.Str("gamma"),
-              ujson.Str("ro")
-            )
-          )
-        }
-      )
-    ) @@ silent
+    )
 }
--- morphir/ir/test/src/morphir/ir/PathSpec.scala
+++ morphir/ir/test/src/morphir/ir/PathSpec.scala
@@ -1,130 +1,16 @@
-/*
-Copyright 2020 Morgan Stanley
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
- */
-
 package morphir.ir
-import zio.test._
+
+import morphir.ir.Path.Path
 import zio.test.Assertion._
-import zio.test.TestAspect._
-import morphir.ir.fuzzer.NameFuzzers._
-import morphir.ir.path.Path
-import morphir.ir.testing.JsonSpec
+import zio.test._
 
-object PathSpec extends DefaultRunnableSpec with JsonSpec {
+object PathSpec extends DefaultRunnableSpec{
+    override def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] = suite("Path Utility Functions Tests") (
+        test("fromString Test"){
+            val pathFromString = Path.fromString("fooBar.baz")
+            val expectedPath : Path = List(List("foo","bar"), List("baz"))
 
-  def spec =
-    suite("PathSpec")(
-      suite("Creating a Path from a String")(
-        test("Should be possible when given a simple string") {
-          assert(Path.fromString("Person"))(
-            equalTo(Path(List(Name.fromString("person"))))
-          )
-        },
-        test("Should be possible when given a dotted string") {
-          assert(Path.fromString("blog.Author"))(
-            equalTo(
-              Path(
-                List(Name.fromList(List("blog")), Name.fromList(List("author")))
-              )
-            )
-          )
+            assert(pathFromString)(equalTo(expectedPath))
         }
-      ),
-      suite("Transforming a Path into a String")(
-        test("Should be supported (a)") {
-          val input =
-            Path(
-              Name("foo", "bar"),
-              Name("baz")
-            )
-
-          assert(Path.toString(Name.toTitleCase, ".", input))(
-            equalTo("FooBar.Baz")
-          )
-        },
-        test("Should be supported (b)") {
-          val input =
-            Path(
-              Name("foo", "bar"),
-              Name("baz")
-            )
-
-          assert(Path.toString(Name.toSnakeCase, "/", input))(
-            equalTo("foo_bar/baz")
-          )
-        }
-      ),
-      suite("Transforming to a list of Names")(
-        test("Should be support via the toList function") {
-          assert(
-            Path.toList(Path(Name("Com", "Example"), Name("Hello", "World")))
-          )(
-            equalTo(List(Name("Com", "Example"), Name("Hello", "World")))
-          )
-        }
-      ),
-      suite("Checking if one Path is a prefix of another should:")(
-        test("""Return true: Given path is "foo/bar" and prefix is "foo" """) {
-          val sut    = Path.fromString("foo/bar")
-          val prefix = Path.fromString("foo")
-
-          assert(Path.isPrefixOf(prefix = prefix, path = sut))(isTrue)
-        },
-        test("""Return false: Given path is "foo/foo" and prefix is "bar" """) {
-          val sut    = Path.fromString("foo/foo")
-          val prefix = Path.fromString("bar")
-
-          assert(Path.isPrefixOf(prefix = prefix, path = sut))(isFalse)
-        },
-        test("""Return true: Given equal paths""") {
-          val sut    = Path.fromString("foo/bar/baz")
-          val prefix = sut
-
-          assert(Path.isPrefixOf(prefix = prefix, path = sut))(isTrue)
-        }
-      ),
-      suite("Path: Encoding and Decoding")(
-        testM(
-          "Path when encoded and then decoded should be equivalent to the original"
-        ) {
-          checkM(Gen.listOfBounded(1, 3)(fuzzName)) { names =>
-            val sut = Path.fromList(names)
-            checkCodecIsWellBehaved(sut)
-          }
-        },
-        checkEncodesTo(
-          Path.fromNames(
-            Name.name("alpha"),
-            Name.name("beta"),
-            Name.name("gamma")
-          ),
-          """[["alpha"],["beta"],["gamma"]]"""
-        ),
-        checkEncodesTo(
-          Path.fromNames(
-            Name.name("alpha", "omega"),
-            Name.name("beta", "delta"),
-            Name.name("gamma")
-          ),
-          """[["alpha","omega"],["beta","delta"],["gamma"]]"""
-        )
-      )
-    ) @@ silent
-
-  def checkEncodesTo(sut: Path, expectedJsonText: String): ZSpec[Any, Nothing] =
-    test(s"Given Path: $sut it should encode to: $expectedJsonText") {
-      assert(compactEncode(sut))(equalTo(expectedJsonText))
-    }
+    )
 }
