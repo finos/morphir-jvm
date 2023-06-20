a2f86e92428243dbcfeaf6fb051256e57733f2c4
klahnunya <98951378+klahnunya@users.noreply.github.com>
2023-06-20 08:40:35 +0000

    fix for scala212-api (#147)

--- morphir/ir/test/src/morphir/ir/DistributionSpec.scala
+++ morphir/ir/test/src/morphir/ir/DistributionSpec.scala
@@ -1,23 +1,25 @@
 package morphir.ir
 
-import io.circe.{ Json, parser }
-import io.circe.Decoder.Result
+import io.circe.Json
+import io.circe.parser.parse
 import morphir.FormatVersionCodecs._
 import org.scalatest.funsuite.AnyFunSuite
-import scala.io.{ BufferedSource, Source }
+import scala.io.Source
 
 class DistributionSpec extends AnyFunSuite {
-  val bufferResource: BufferedSource = Source.fromFile(
-    "morphir/ir/test/resources/morphir/ir/morphir-ir.json"
-  )
-  val irContent: String = bufferResource.mkString
-  bufferResource.close()
 
-  test("Decoding and Encoding an IR JSON should match the original JSON") {
-    val irJson = parser.parse(irContent)
-    val distributionResult: Result[morphir.ir.Distribution.Distribution] =
-      decodeDistributionVersion.decodeJson(irJson.getOrElse(Json.Null))
-    val encodedDistributionResult = distributionResult.map(encodeDistributionVersion.apply)
-    assert(encodedDistributionResult === irJson)
+  val irJson = Source.fromFile("morphir/ir/test/resources/morphir/ir/morphir-ir.json")
+
+  test("Decoding and Encoding an IR JSON should match the original decoded JSON") {
+    val parsedIr  = parse(irJson.mkString("")).getOrElse(Json.Null)
+    val decodedIr = decodeDistributionVersion(parsedIr.hcursor)
+
+    val encodeIr      = decodedIr.map(encodeDistributionVersion.apply)
+    val encodedIrJson = encodeIr.getOrElse(Json.Null)
+
+    val parsedJson    = parse(encodedIrJson.toString())
+    val decodedResult = decodeDistributionVersion.decodeJson(parsedJson.getOrElse(Json.Null))
+
+    assert(decodedResult === decodedIr)
   }
 }
