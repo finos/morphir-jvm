c3e3b6fb33498505807f1bd1858eba29200f2634
Scala Steward <scala_steward@virtuslab.com>
2025-01-16 16:53:14 +0000

    Update scala-library to 2.13.16

--- mill-build/src/millbuild/deps.scala
+++ mill-build/src/millbuild/deps.scala
@@ -33,7 +33,7 @@ object Deps {
     }
 
     case object `47Deg` {
-        val memeid4s = ivy"com.47deg::memeid4s:${Versions.`memeid4s`}"
+      val memeid4s = ivy"com.47deg::memeid4s:${Versions.`memeid4s`}"
     }
 
     case object geirsson {
@@ -272,7 +272,7 @@ object ScalaVersions {
   import DevMode._
   val all      = if (devMode) Seq(scala3x) else Seq(scala213, scala3x)
   def scala212 = "2.12.20"
-  def scala213 = "2.13.15"
+  def scala213 = "2.13.16"
   def scala3x  = "3.3.4"
 
   def scalaJSVersion     = "1.17.0"
