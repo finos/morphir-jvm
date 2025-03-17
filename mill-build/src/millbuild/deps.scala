ef1e9f92018ade8920b41216efd4a0dd3bd2fab4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-03-17 23:23:14 +0100

    Update scala-collection-compat to 2.13.0 (#188)
    Co-authored-by: Damian Reeves <957246+DamianReeves@users.noreply.github.com>

--- mill-build/src/millbuild/deps.scala
+++ mill-build/src/millbuild/deps.scala
@@ -185,7 +185,7 @@ object Deps {
         ivy"org.scala-lang::scala3-tasty-inspector::$scalaVersion"
 
       case object modules {
-        val `scala-collection-compat` = ivy"org.scala-lang.modules::scala-collection-compat::2.12.0"
+        val `scala-collection-compat` = ivy"org.scala-lang.modules::scala-collection-compat::2.13.0"
       }
     }
     case object scalameta {
