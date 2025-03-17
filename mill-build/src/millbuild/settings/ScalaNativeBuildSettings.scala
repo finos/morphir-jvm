fd288a4076d1f56db6183db93ab3bc125ddb1b01
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2025-03-17 18:40:19 -0500

    Update Scala 3 to Scala 3.3.5 and properly upgrade Scala Native

--- mill-build/src/millbuild/settings/ScalaNativeBuildSettings.scala
+++ mill-build/src/millbuild/settings/ScalaNativeBuildSettings.scala
@@ -12,7 +12,7 @@ object ScalaNativeBuildSettings {
   val config: Config[ScalaNativeBuildSettings] = deriveConfig[ScalaNativeBuildSettings]
   lazy val default: ScalaNativeBuildSettings   = ScalaNativeBuildSettings()
 
-  lazy val defaultVersion = "0.5.5"
+  lazy val defaultVersion = "0.5.7"
 
   implicit lazy val rw: upickle.default.ReadWriter[ScalaNativeBuildSettings] = upickle.default.macroRW
 }
